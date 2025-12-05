8441bb6a45766e930c03c0fcaccba2738315875b
rkdms6767 <rkdms5991@naver.com>
2025-12-05 13:24:14 +0900

    Project Start

---
+++ service/like/src/main/java/gganii/board/like/likeApplication.java
@@ -0,0 +1,11 @@
+package gganii.board.like;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class likeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(likeApplication.class, args);
+    }
+}
---
+++ service/view/src/main/java/gganii/board/view/viewApplication.java
@@ -0,0 +1,11 @@
+package gganii.board.view;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class viewApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(viewApplication.class, args);
+    }
+}
